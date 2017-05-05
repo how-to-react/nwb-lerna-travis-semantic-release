2bf5660fe9f85e3bbb927071ddc93bc1b1ec46f7
Stephen Fraser <stephen@blackgrid.co.uk>
2017-05-05 20:44:51 +0100

    feat($build): fixing more parts of build
    affects: @how-to-react/lerna-semr-nwb-react-app, @how-to-react/lerna-semr-nwb-web-module

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -28,15 +28,23 @@ if [[ ${TRAVIS_BRANCH} == 'master' ]]; then
   git clone https://github.com/${TRAVIS_REPO_SLUG}.git ${TRAVIS_REPO_SLUG}
   git checkout $TRAVIS_BRANCH
 
+  echo "Username: ${RELEASE_GH_USERNAME}";
+
   git config credential.helper store
   echo "https://${RELEASE_GH_USERNAME}:${RELEASE_GH_TOKEN}@github.com/how-to-react/nwb-lerna-travis-semantic-release.git" > ~/.git-credentials
 
+
+  echo "Token length ${#NPM_TOKEN}";
   npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN} -q
+
+  echo "Github: ${RELEASE_GH_USERNAME} ${RELEASE_GH_EMAIL}"
 
   git config --global user.email ${RELEASE_GH_EMAIL}
   git config --global user.name ${RELEASE_GH_USERNAME}
   git config --global push.default simple
 
+
+  echo "Fetching tags..."
   git fetch --tags
   git branch -u origin/${TRAVIS_BRANCH}
   git fsck --full #debug
